82ea62a6404eb203c59c433a8bbfe501729178e0
thekightkeeper1 <ty.singh828@gmail.com>
2026-02-05 04:37:53 -0500

    You can get the lib files now lol

---
+++ get-lib.bat
@@ -0,0 +1,2 @@
+curl -L -O "https://github.com/Ciantic/VirtualDesktopAccessor/releases/download/2024-12-16-windows11/VirtualDesktopAccessor.dll"
+move VirtualDesktopAccessor.dll .\lib\VirtualDesktopAccessor.dll
